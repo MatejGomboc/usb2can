d82c074fd9c07537cb7cc22aebbbb524754aa8a0
Matej Gomobc <matejg03@gmail.com>
2025-02-15 16:38:16 +0100

    Fixed line indentations.

--- firmware/vectors_table.cpp
+++ firmware/vectors_table.cpp
@@ -25,36 +25,36 @@ void (* const vectors_table[])() = {
     unhandledIrqIsr,
     unhandledIrqIsr, // pendSv
     unhandledIrqIsr, // sysTick
-    unhandledIrqIsr, // Window WatchDog              
-    unhandledIrqIsr, // PVD through EXTI Line detect 
-    unhandledIrqIsr, // RTC through the EXTI line    
-    unhandledIrqIsr, // FLASH                        
-    unhandledIrqIsr, // RCC & CRS                    
-    unhandledIrqIsr, // EXTI Line 0 and 1            
-    unhandledIrqIsr, // EXTI Line 2 and 3            
-    unhandledIrqIsr, // EXTI Line 4 to 15            
-    unhandledIrqIsr, // USB, UCPD1, UCPD2            
-    unhandledIrqIsr, // DMA1 Channel 1               
-    unhandledIrqIsr, // DMA1 Channel 2 and Channel 3 
-	unhandledIrqIsr, // DMA1 Ch4 to Ch7, DMA2 Ch1 to Ch5, DMAMUX1 overrun 
-    unhandledIrqIsr, // ADC1, COMP1 and COMP2         
-    unhandledIrqIsr, // TIM1 Break, Update, Trigger and Commutation 
-    unhandledIrqIsr, // TIM1 Capture Compare         
-    unhandledIrqIsr, // TIM2                         
-    unhandledIrqIsr, // TIM3, TIM4                   
-    unhandledIrqIsr, // TIM6, DAC and LPTIM1         
-    unhandledIrqIsr, // TIM7 and LPTIM2              
-    unhandledIrqIsr, // TIM14                        
-    unhandledIrqIsr, // TIM15                        
-    unhandledIrqIsr, // TIM16 & FDCAN1_IT0 & FDCAN2_IT0 
-    unhandledIrqIsr, // TIM17 & FDCAN1_IT1 & FDCAN2_IT1 
-    unhandledIrqIsr, // I2C1                         
-    unhandledIrqIsr, // I2C2, I2C3                   
-    unhandledIrqIsr, // SPI1                         
-    unhandledIrqIsr, // SPI2, SPI3                   
-    unhandledIrqIsr, // USART1                       
-    unhandledIrqIsr, // USART2 & LPUART2             
-    unhandledIrqIsr, // USART3, USART4, USART5, USART6, LPUART1   
-    unhandledIrqIsr, // CEC                          
-    unhandledIrqIsr  // AES and RNG                  
+    unhandledIrqIsr, // Window WatchDog
+    unhandledIrqIsr, // PVD through EXTI Line detect
+    unhandledIrqIsr, // RTC through the EXTI line
+    unhandledIrqIsr, // FLASH
+    unhandledIrqIsr, // RCC & CRS
+    unhandledIrqIsr, // EXTI Line 0 and 1
+    unhandledIrqIsr, // EXTI Line 2 and 3
+    unhandledIrqIsr, // EXTI Line 4 to 15
+    unhandledIrqIsr, // USB, UCPD1, UCPD2
+    unhandledIrqIsr, // DMA1 Channel 1
+    unhandledIrqIsr, // DMA1 Channel 2 and Channel 3
+	unhandledIrqIsr, // DMA1 Ch4 to Ch7, DMA2 Ch1 to Ch5, DMAMUX1 overrun
+    unhandledIrqIsr, // ADC1, COMP1 and COMP2
+    unhandledIrqIsr, // TIM1 Break, Update, Trigger and Commutation
+    unhandledIrqIsr, // TIM1 Capture Compare
+    unhandledIrqIsr, // TIM2
+    unhandledIrqIsr, // TIM3, TIM4
+    unhandledIrqIsr, // TIM6, DAC and LPTIM1
+    unhandledIrqIsr, // TIM7 and LPTIM2
+    unhandledIrqIsr, // TIM14
+    unhandledIrqIsr, // TIM15
+    unhandledIrqIsr, // TIM16 & FDCAN1_IT0 & FDCAN2_IT0
+    unhandledIrqIsr, // TIM17 & FDCAN1_IT1 & FDCAN2_IT1
+    unhandledIrqIsr, // I2C1
+    unhandledIrqIsr, // I2C2, I2C3
+    unhandledIrqIsr, // SPI1
+    unhandledIrqIsr, // SPI2, SPI3
+    unhandledIrqIsr, // USART1
+    unhandledIrqIsr, // USART2 & LPUART2
+    unhandledIrqIsr, // USART3, USART4, USART5, USART6, LPUART1
+    unhandledIrqIsr, // CEC
+    unhandledIrqIsr  // AES and RNG
 };
